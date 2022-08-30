cfaa28b6d26bd2d981786def5b04ddf7861ced42
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-30 22:41:33 +0500

    HomeTask008.3 completed.

--- HomeTask008/HomeTask008.3/Program.cs
+++ HomeTask008/HomeTask008.3/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CW(string msg)
+{
+  Console.WriteLine(msg);
+}
+void CWR(string msg)
+{
+  Console.Write(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+try
+{
+  CW("Задание 3. Программа, которая составляет частотный словарь элементов двумерного массива. Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.");
+
+  int rows = new Random().Next(1, 6);
+  int columns = new Random().Next(1, 6);
+  int[,] array = new int[rows, columns];
+  int[] array2 = new int[rows * columns];
+  int index = 0;
+  int count = 1;
+  int prev = array2[0];
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      array[i, j] = new Random().Next(0, 10);
+      CWR(array[i, j].ToString() + "  ");
+      array2[index] = array[i, j];
+      index++;
+    }
+    CW("");
+  }
+
+  for (int i = 0; i < array2.Length; i++)
+  {
+    for (int j = 0; j < array2.Length - 1; j++)
+    {
+      if (array2[j] > array2[j + 1])
+      {
+        int count2 = array2[j];
+        array2[j] = array2[j + 1];
+        array2[j + 1] = count2;
+      }
+    }
+  }
+
+  for (int i = 1; i < array2.Length; i++)
+  {
+    if (prev == array2[i]) count++;
+    else
+    {
+      CW($"{prev} встречается {count} раз");
+      prev = array2[i];
+      count = 1;
+    }
+  }
+}
+catch (InvalidCastException e) { }
