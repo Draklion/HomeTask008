212085e5e19bcc8bb3790ebcce87141229ebdb73
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-31 10:42:38 +0500

    HomeTask008.5 completed.

--- HomeTask008/HomeTask008.5/Program.cs
+++ HomeTask008/HomeTask008.5/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void CW(string msg)
+{
+  Console.WriteLine(msg);
+}
+void CWR(string msg)
+{
+  Console.Write(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+try
+{
+  CW("Задание 5. Программа, которая упорядочит по убыванию элементы каждой строки двумерного массива.");
+
+  int rows = new Random().Next(1, 6);
+  int columns = new Random().Next(1, 6);
+  int[,] array = new int[rows, columns];
+  CW("Исходный массив:");
+
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < columns; j++)
+    {
+      array[i, j] = new Random().Next(0, 10);
+      CWR(array[i, j].ToString() + "  ");
+    }
+    CW("");
+  }
+
+  for (int i = 0; i < array.GetLength(0) * array.GetLength(1); i++)
+  {
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[j, k] > array[j, k + 1])
+        {
+          int count = array[j, k];
+          array[j, k] = array[j, k + 1];
+          array[j, k + 1] = count;
+        }
+      }
+    }
+  }
+  CW("");
+  CW("Отсортированный массив:");
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      CWR(array[i, j].ToString() + "  ");
+    }
+    CW("");
+  }
+}
+catch (InvalidCastException e) { }
