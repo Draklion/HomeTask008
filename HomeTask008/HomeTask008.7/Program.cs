1c7be65eae72584921634ac8c88bb3897f47b015
«Dmitry_Machnev» <draklion56@gmail.com>
2022-09-01 23:47:40 +0500

    Added files.

---
+++ HomeTask008/HomeTask008.7/Program.cs
@@ -0,0 +1,11 @@
+﻿string number = Console.ReadLine();
+int sum = 0;
+void Sum(string number, int i = 0)
+{
+  if (number.Length == i)
+  {
+    sum = sum + Int32.Parse(number[i].ToString());
+    Sum(number, i++);
+  }
+  Console.WriteLine(sum);
+}
