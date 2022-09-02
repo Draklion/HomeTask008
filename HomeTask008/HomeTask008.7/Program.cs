083e3aa65509c337d966dc4ac5a43a0510a72d25
«Dmitry_Machnev» <draklion56@gmail.com>
2022-09-02 10:32:01 +0500

    HomeTask008.6 completed.

--- HomeTask008/HomeTask008.7/Program.cs
+++ HomeTask008/HomeTask008.7/Program.cs
@@ -1,11 +1,32 @@
-﻿string number = Console.ReadLine();
-int sum = 0;
-void Sum(string number, int i = 0)
+﻿void CW(string msg)
 {
-  if (number.Length == i)
-  {
-    sum = sum + Int32.Parse(number[i].ToString());
-    Sum(number, i++);
-  }
-  Console.WriteLine(sum);
+  Console.WriteLine(msg);
 }
+void CWR(string msg)
+{
+  Console.Write(msg);
+}
+string CR()
+{
+  return Console.ReadLine();
+}
+
+try
+{
+  CW("Задание 7. Программа, которая будет построчно выводить массив, добавляя индексы каждого элемента.");
+
+  int[,,] Matrix = new int[2, 2, 2] {
+    { {10,74},{47,425} },
+    { {5,71}, {12,75} }
+    };
+
+  for (int i = 0; i < Matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < Matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < Matrix.GetLength(2); k++) CWR($"{Matrix[i, j, k]}({i}{j}{k}) ");
+    }
+    CW("");
+  }
+}
+catch (InvalidCastException e) { }
